67768fd8f8df39b3de9cd60ea4c2ebced5f86d28
jrubin22 <jrubin22@binghamton.edu>
2021-03-18 15:25:02 -0400

    Test Scene w/ Movement

---
+++ What-The-Shell/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator thisAnim;
+    private Rigidbody2D rigid;
+    public LayerMask whatIsGround;
+    public Vector3 speed = new Vector3(1, 1, 1);
+    // Start is called before the first frame update
+    void Start()
+    {
+        thisAnim = GetComponent<Animator>();
+        rigid = GetComponent<Rigidbody2D>();
+      
+    }     
+    
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(speed.x * h, 0, 0);
+        movement *= Time.deltaTime;
+        thisAnim.SetFloat("Speed", Mathf.Abs(h));
+
+        if (h < 0.0)
+        {
+            transform.localScale = new Vector3(-6, 6, 1);
+            transform.Translate(movement);
+        }
+        else if (h > 0.0)
+        {
+            transform.localScale = new Vector3(6, 6, 1);
+            transform.Translate(movement);
+        }
+
+        if (Input.GetButtonDown("Horizontal"))
+        {
+            thisAnim.SetTrigger("Walk");
+        }
+
+    }
+}
