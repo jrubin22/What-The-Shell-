7af16a67cb29af22b23165d3d74e9bf9a3f78aa3
JanathanGame <johnsantare1@gmail.com>
2021-03-19 22:04:36 -0400

    CameraSwitch and Other Basics

---
+++ What-The-Shell/Assets/Scripts/CamSwitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamSwitch : MonoBehaviour
+{
+    public GameObject Camera1;
+    public GameObject Camera2;
+
+    void Update()
+    {
+        if (Input.GetButton("MapCamera"))
+        {
+            Camera1.SetActive(false);
+            Camera2.SetActive(true);
+        }
+        else
+        {
+            Camera1.SetActive(true);
+            Camera2.SetActive(false);
+        }
+    }
+}
