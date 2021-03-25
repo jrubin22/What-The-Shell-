4eb7fa08d99abbae6d5c2ae2d29ff4c89729dc57
JanathanGame <johnsantare1@gmail.com>
2021-03-24 21:27:52 -0400

    TextContinued

---
+++ What-The-Shell/Assets/Scripts/TextManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextManager : MonoBehaviour
+{
+    public Text textText;
+
+    private Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartText (Text text)
+    {
+        Debug.Log("Showing Message");
+
+        sentences.Clear();
+
+        foreach (string sentence in text.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence ()
+    {
+        if (sentences.Count == 0)
+        {
+            EndText();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        textText.text = sentence;
+    }
+
+    void EndText()
+    {
+        Debug.Log ("End of Message")
+    }
+
+}
---
+++ What-The-Shell/Assets/Scripts/TextTrigger.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextTrigger : MonoBehaviour
+{
+    public Text text;
+
+    public void TriggerText ()
+    {
+        FindObjectOfType<TextManager>().StartText(text)
+    }
+}
